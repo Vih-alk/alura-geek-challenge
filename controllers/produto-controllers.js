4de9a88f22f1aeb17940ff2217ed21c44cd3bac4
Vívian <vvianalkmimalves@gmail.com>
2023-10-02 14:22:32 -0300

    Atualização

--- controllers/produto-controllers.js
+++ controllers/produto-controllers.js
@@ -1,16 +1,42 @@
-const novoProduto = (nome, imagemURL, id, preco) => {
+import { produtoService } from "../services/produtos-service.js";
+
+const novoProduto = ( imageURL, alt, name, price, id ) => {
    const card = document.createElement("div")
+   
    const conteudo = `
-   <div class= "produto">
-        <img src="${imagemURL}" alt"">
-        <h1 class="produto-nome">${nome}</h1>
-        <p class="produto-preco">${preco}</p>
-        <a class="ver-produto">${}</a>
+   
+   <div class= "produto__box">
+        <img src="${imageURL}" alt"${alt}" class= "produto__img">
+        <h1 class="produto-nome" class= "produto__item">${name}</h1>
+        <p class="produto-preco" class= "produto__preco">${price}</p>
+        <a class="ver-produto" href = "/pages/produto.html?id${id}">Ver produto</a>
    </div>
    `
-   card.innerHTML = conteudo
+  card.innerHTML = conteudo
 
    return card
+
+};
+
+const produtos = document.querySelector('[data-produto]');
+
+const render = async () => {
+   try {
+      const produtosListados = await produtoService.produtosListados()
+      produtosListados.forEach((elemento) => {
+         produtos.appendChild(
+            novoProduto(
+               elemento.imageURL,
+               elemento.alt,
+               elemento.name,
+               elemento.price,
+               elemento.id
+            )
+         )
+      });
+   } catch (error) {
+      console.log(error)
+   }
 }
 
-const produtos = document.querySelector("[data-produto]")
+render();
--- services/produtos-service.js
+++ services/produtos-service.js
@@ -1,11 +1,39 @@
 //GET
 
-const listaProdutos = () => {
-    fetch("http://localhost:3000/produto")
-    .then(resposta => resposta.json())
-    .catch(error => console.log(error))
+const produtosListados = () => {
+    return fetch('http://localhost:3000/produto')
+    .then((resposta) => resposta.json())
+    .catch((error) => console.log(error))
+
 }
 
-export const produtosServicos = {
-   listaProdutos
+//POST
+
+const criaProduto = ( imageURL, section, name, price, description ) => {
+    return fetch('http://localhost:3000/produto', {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+        imageURL,
+        section,
+        name,
+        price,
+        description
+    })
+
+    })
+    .then(resposta => {
+        if (resposta.ok) {
+         return resposta.body   
+        }
+        throw new Error("Não foi possível adicionar o produto.")
+    })
+
+}
+
+export const produtoService = {
+    produtosListados,
+    criaProduto
 }
